be186becef8b001b2f3c840b39b90f8335a80518
Sandeepjoe27 <fatherpiosaint@gmail.com>
2024-09-26 21:04:15 +0530

    first updates

---
+++ README.md
@@ -0,0 +1,8 @@
+## important commands
+
+1.npm install
+2.npm run serve
+3.git init
+4.git add .
+5.git commit -m "your msg"
+6.git push -u origin main or git push
---
+++ main.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express()
+const port =6969;
+
+app.get('/',(req,res)=>{
+    res.json({msg:"hello"});
+})
+
+app.listen(port, ()=>{
+    console.log(`the server is running at http://localhost:${port}`);
+});
